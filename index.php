adebe931a713ad796ba6a286cba8d620a69b10c8
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-22 08:50:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-<h1>POO Exercises</h1>
-
 <?php
 
-spl_autoload_register(function ($class_name) {
-    require $class_name . '.php';
-});
+require 'Auteur.php';
+require 'Livre.php';
+
+$stephenKing = new Auteur('King', 'Stephen');
 
 
-$stephenking = new Auteur("King","Stephen");
+$ca = new Livre('Ca', 1138, 1986, 20, $stephenKing);
+$simetierre = new Livre('Simetierre', 374, 1983, 15, $stephenKing);
+$leFleau = new Livre('Le Fleau', 823, 1978, 14, $stephenKing);
+$shining = new Livre('Shining', 447, 1977, 16, $stephenKing);
 
-$ca = new Livre("Ca","1138","1986","20e");
-$Simetierre = new Livre("Simetierre","374","1983","15e");
-$LeFleau = new Livre("Le fleau","823","1978","14e");
-$Shining = new Livre("Shining","447","1977","16e");
-
-
-echo $stephenking->afficherBibliographie();
-
+echo $stephenKing->afficherBibliographie();
 
