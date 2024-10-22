ade12b02952c52cea25d7487fbddb95f3d2a89ad
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-22 08:51:10 +0200

    Update Livre.php

--- Livre.php
+++ Livre.php
@@ -1,94 +1,76 @@
 <?php
-
-
-
-class Livre{
-    private string $Titre;
-    private string $NbPages;
-    private DateTime $AnneParution;
-    private string $Prix;
-
-
-    public function __construct(string $Titre, string $NbPages,string $AnneParution,string $Prix){
-        $this->Titre = $Titre;
-        $this->NbPages = $NbPages;
-        $this->AnneParution = new DateTime($AnneParution);
-        $this->Prix = $Prix;
-    }
-
-
+class Livre {
+   private string $titre;
+   private int $nombrePages;
+   private int $anneeParution;
+   private float $prix;
+   private Auteur $auteur;
    
-    public function getTitre():string
-    {
-        return $this->Titre;
-    }
-
-   
-    public function setTitre($Titre)
-    {
-        $this->Titre = $Titre;
-
-        return $this;
-    }
-
-    
-    public function getNbPages()
-    {
-        return $this->NbPages;
-    }
-
-    
-    public function setNbPages($NbPages)
-    {
-        $this->NbPages = $NbPages;
-
-        return $this;
-    }
-
-    
-    public function getAnneParution():DateTime
-    {
-        return $this->AnneParution;
-    }
-
-    
-    public function setAnneParution($AnneParution)
-    {
-        $this->AnneParution = $AnneParution->format("Y");
-
-        return $this;
-    }
-
-    
-    public function getPrix()
-    {
-        return $this->Prix;
-    }
-
-    
-    public function setPrix($Prix)
-    {
-        $this->Prix = $Prix;
-
-        return $this;
-    }
-
-
-    public function afficherBibliographie(){
-        $result = "<h2>Livres de $this</h2>";
-
-
-        foreach ($this->Livre as $livre){
-             $result .= $Titre->getTitre()."(".$AnneParution->getAnneParution().") :".$NbPages->getNbPages()."pages /".$Prix->getPrix()."€ <br>";
-        }
-
-        return $result;
+   public function __construct(string $titre, int $nombrePages, int $anneeParution, float $prix, Auteur $auteur) {
+       $this->titre = $titre;
+       $this->nombrePages = $nombrePages;
+       $this->anneeParution = $anneeParution;
+       $this->prix = $prix;
+       $this->auteur = $auteur;
+       $this->auteur->ajouterLivre($this);
+ 
    }
 
-
-    public function __toString(){
-        return $this->Titre;
+   public function getAuteur() : Auteur {
+    return $this-> auteur;
    }
 
+   public function setAuteur($auteur){
+      $this->auteur = $auteur;
+     return $this;
+   }
 
-}
+   public function getTitre(){
+      return $this->titre;
+   }
+
+   public function setTitre($titre){
+      $this->titre = $titre;
+
+      return $this;
+   }
+ 
+   public function getNombrePages(){
+      return $this->nombrePages;
+   }
+
+   public function setNombrePages($nombrePages){
+      $this->nombrePages = $nombrePages;
+
+      return $this;
+   }
+
+   public function getAnneeParution(){
+      return $this->anneeParution;
+   }
+ 
+   public function setAnneeParution($anneeParution)
+   {
+      $this->anneeParution = $anneeParution;
+
+      return $this;
+   }
+
+   public function getPrix()
+   {
+      return $this->prix;
+   }
+
+   public function setPrix($prix)
+   {
+      $this->prix = $prix;
+
+      return $this;
+   }
+
+   public function __toString(): string {
+      return "{$this->titre} ({$this->anneeParution}) : {$this->nombrePages} pages / {$this->prix}€ <br>";
+  }
+
+   }
+
